75998962728645d70d6d379ab6c064ade0d20e7e
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-06-21 17:43:39 +0530

    prepare for vercel deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
-import React from "react";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 interface HeroProps {
